4991a43a5015be90d7cabb0ac9d5e95a912af79d
Zackyinnova <zackyinova1@gmail.com>
2026-04-15 21:08:12 +0700

    new update for card shoppage.html

--- static/js/shoppage.js
+++ static/js/shoppage.js
@@ -10,3 +10,20 @@ buttonNavShop.forEach((btn, index) =>{
 });
 
 
+const buttonColor = document.querySelectorAll(".main-color");
+const textColor = document.querySelectorAll(".text-taste");
+const imgProduct = document.querySelectorAll(".img-product")
+
+buttonColor.forEach((buttonColor, index) => {
+    buttonColor.addEventListener('click', () =>{
+
+        textColor.forEach (textColor => textColor.classList.remove('show'));
+
+        textColor[index].classList.add('show');
+
+        imgProduct.forEach(imgProduct => imgProduct.classList.remove('show'));
+        imgProduct[index].classList.add('show');
+    });
+});
+
+
