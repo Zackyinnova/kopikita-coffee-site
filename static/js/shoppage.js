0a5ea5defbda60e9fbe4eface1fd3e5b4b1d1b89
Zackyinnova <zackyinova1@gmail.com>
2026-04-22 16:23:03 +0700

    new update for add the login page

--- static/js/shoppage.js
+++ static/js/shoppage.js
@@ -44,6 +44,41 @@ cards.forEach(card => {
     });
 });
 
+const ContentFooter = document.querySelectorAll(".content-footer");
+
+ContentFooter.forEach((item) =>{
+    item.addEventListener("click", ()=>{
+
+        const overlayFooter = item.querySelector(".overlay-content-footer");
+
+        document.querySelectorAll(".overlay-content-footer").forEach((el) =>{
+            if(el !== overlayFooter){
+                el.style.display = "none";
+            }
+        })
+
+        if(overlayFooter.style.display === "block"){
+            overlayFooter.style.display = "none";
+        }else{
+            overlayFooter.style.display = "block";
+        }
+    });
+});
+
+const overlayNav = document.getElementById("overlay-navbar");
+const burgerMenu = document.getElementById("icon-burger-menu");
+
+burgerMenu.addEventListener("click", () =>{
+    overlayNav.style.display = "flex";
+});
+
+overlayNav.addEventListener("click", (e) =>{
+    if(e.target === overlayNav){
+        overlayNav.style.display = "none";
+    }
+});
+
+
 
 
 
