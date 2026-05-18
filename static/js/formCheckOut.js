0b482bbab2bfe056342246ecf54ffd646e748e4e
Zackyinnova <zackyinova1@gmail.com>
2026-05-18 20:33:23 +0700

    Feat: add dynamic shipping and grand total calculation on checkout page

--- static/js/formCheckOut.js
+++ static/js/formCheckOut.js
@@ -1,6 +1,11 @@
 //count price all item
 const priceAllItem = document.querySelector(".price-all-item");
 
+const shippingSelect = document.querySelector("#select-shipping");
+const shippingPrice = document.querySelector(".shipping-price");
+
+const totalPrice = document.querySelector(".number-total-price");
+
 function formatRupiah(angka){
     return angka.toLocaleString("id-ID");
 }
@@ -16,6 +21,31 @@ function UpdatePriceItem(){
     });
 
     priceAllItem.textContent = "Rp." + formatRupiah(totalPriceItem);
+
+    updateGrangTotal(totalPriceItem);
 }
+
+function updateGrangTotal(subtotal){
+    const shipping = parseInt(shippingSelect.value) || 0;
+
+    shippingPrice.textContent = "Rp." + formatRupiah(shipping);
+
+    const grandTotal = subtotal + shipping;
+
+    totalPrice.textContent = "Rp." + formatRupiah(grandTotal);
+}
+
+shippingSelect.addEventListener("change", () => {
+
+    // ambil subtotal dari text
+    const subtotal = parseInt(
+        priceAllItem.textContent
+            .replace("Rp.", "")
+            .replace(/\./g, "")
+    );
+
+    updateGrangTotal(subtotal);
+});
+
 
 UpdatePriceItem();
